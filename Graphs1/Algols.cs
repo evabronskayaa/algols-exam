947d6cb496768a5a6119b09d6098102f77de35bf
evabronskayaa <eva27122002b@gmail.com>
2022-01-06 14:41:35 +0500

    added graph algorithms

--- Graphs1/Algols.cs
+++ Graphs1/Algols.cs
@@ -1,167 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
-using Graphs1;
+using System.Linq;
 
 namespace Graphs1
 {
-    public class Algols
+    public class DijkstraData
     {
-        public static void DepthTraversal(Graph graph)
+        public Node Previous { get; set; }
+        public double Price { get; set; }
+    }
+
+    public static class Algols
+    {
+        public static List<Node> Dijkstra(Graph graph, Dictionary<Edge, double> weights, Node start, Node end)
         {
-            List<Vertex> vertex = new List<Vertex>();
-            List<EdgeData> bridges = new List<EdgeData>();
-            Stack<Vertex> stack = new Stack<Vertex>();
-            stack.Push(graph.Vertexes[0]);
-            //?
-            GraphVisualizationData data = new GraphVisualizationData(graph);
+            var notVisited = graph.Nodes.ToList();
+            var track = new Dictionary<Node, DijkstraData>();
+            track[start] = new DijkstraData {Price = 0, Previous = null};
 
-            Visualizer.CreateBitmaps(data);
-            Visualizer.AddLog("Кладём в стек первую же вершину");
+            while (true)
+            {
+                Node toOpen = null;
+                var bestPrice = double.PositiveInfinity;
+                foreach (var e in notVisited)
+                {
+                    if (track.ContainsKey(e) && track[e].Price < bestPrice)
+                    {
+                        bestPrice = track[e].Price;
+                        toOpen = e;
+                    }
+                }
 
+                if (toOpen == null) return null;
+                if (toOpen == end) break;
+
+                foreach (var e in toOpen.IncidentEdges.Where(z => z.From == toOpen))
+                {
+                    var currentPrice = track[toOpen].Price + weights[e];
+                    var nextNode = e.OtherNode(toOpen);
+                    if (!track.ContainsKey(nextNode) || track[nextNode].Price > currentPrice)
+                    {
+                        track[nextNode] = new DijkstraData {Previous = toOpen, Price = currentPrice};
+                    }
+                }
+
+                notVisited.Remove(toOpen);
+            }
+
+            var result = new List<Node>();
+            while (end != null)
+            {
+                result.Add(end);
+                end = track[end].Previous;
+            }
+
+            result.Reverse();
+            return result;
+        }
+
+        public static IEnumerable<Node> DepthSearch(this Node startNode)
+        {
+            var visited = new HashSet<Node>();
+            var stack = new Stack<Node>();
+            stack.Push(startNode);
             while (stack.Count != 0)
             {
-                var point = stack.Pop();
-                Visualizer.AddLog("Достаём из стека вершину " + (point.Number + 1).ToString());
-
-                if (!vertex.Contains(point))
-                {
-                    Visualizer.AddLog("Эту вершину ещё не посещали");
-                    vertex.Add(point);
-                }
-
-                foreach (var edge in point.Edges)
-                {
-                    if (!vertex.Contains(edge.DestinationVertex))
-                    {
-                        Visualizer.AddLog("Находим вершину-соседа " + (edge.DestinationVertex.Number + 1).ToString()
-                                                                    + " и кладём в стек");
-                        stack.Push(edge.DestinationVertex);
-                        bridges.Add(edge.EdgeDataObject);
-                    }
-                }
+                var node = stack.Pop();
+                if (visited.Contains(node)) continue;
+                visited.Add(node);
+                yield return node;
+                foreach (var incidentNode in node.IncidentNodes)
+                    stack.Push(incidentNode);
             }
         }
 
-        public static void BreadthTraversal(Graph graph)
+        public static IEnumerable<Node> BreadthSearch(this Node startNode)
         {
-            List<Vertex> vertex = new List<Vertex>();
-            var queue = new Queue<Vertex>();
-            queue.Enqueue(graph.Vertexes[0]);
+            var visited = new HashSet<Node>();
+            var queue = new Queue<Node>();
+            queue.Enqueue(startNode);
             while (queue.Count != 0)
             {
-                var point = queue.Dequeue();
-                if (!vertex.Contains(point))
-                {
-                    vertex.Add(point);
-                }
-
-                foreach (var edge in point.Edges)
-                {
-                    if (!vertex.Contains(edge.DestinationVertex))
-                    {
-                        queue.Enqueue(edge.DestinationVertex);
-                    }
-                }
+                var node = queue.Dequeue();
+                if (visited.Contains(node)) continue;
+                visited.Add(node);
+                yield return node;
+                foreach (var incidentNode in node.IncidentNodes)
+                    queue.Enqueue(incidentNode);
             }
-        }
-
-        public static bool BFS(Graph graph, Vertex source, Vertex sink,
-            ref Vertex[] parents)
-        {
-            List<Vertex> vertex = new List<Vertex>();
-            var queue = new Queue<Vertex>();
-            queue.Enqueue(graph.Vertexes[0]);
-            while (queue.Count != 0)
-            {
-                var point = queue.Dequeue();
-                if (!vertex.Contains(point))
-                {
-                    vertex.Add(point);
-
-                    foreach (var edge in point.Edges)
-                    {
-                        if (!vertex.Contains(edge.DestinationVertex))
-                        {
-                            if (edge.DestinationVertex == sink)
-                            {
-                                parents[point.Number] = sink;
-                                return true;
-                            }
-
-                            queue.Enqueue(edge.DestinationVertex);
-                            parents[point.Number] = edge.DestinationVertex;
-                        }
-                    }
-                }
-            }
-
-            return false;
-        }
-
-        //public static void FordFulkerson(GraphVisualizationData graph)
-        //{
-        //    Vertex source = graph.RawGraph.Vertexes[0];
-        //    Vertex sink = graph.RawGraph.Vertexes[graph.RawGraph.Vertexes.Count-1];
-
-        public static int FordFulkerson(GraphVisualizationData graph, Vertex source, Vertex sink)
-        {
-            // Create a residual graph and fill
-            // the residual graph with given
-            // capacities in the original graph as
-            // residual capacities in residual graph
-
-            // Residual graph where rGraph[i,j]
-            // indicates residual capacity of
-            // edge from i to j (if there is an
-            // edge. If rGraph[i,j] is 0, then
-            // there is not)
-
-            // This array is filled by BFS and to store path
-            Vertex[] parents = new Vertex[graph.RawGraph.Vertexes.Count];
-            //foreach (EdgeData t in graph.RawGraph.Edges)
-            //{
-            //    t.Capacity = t.Weight;
-            //}
-            int max_flow = 0; // There is no flow initially
-
-            // Augment the flow while there is path from source
-            // to sink
-            while (BFS(graph.RawGraph, source, sink, ref parents))
-            {
-                // Find minimum residual capacity of the edhes
-                // along the path filled by BFS. Or we can say
-                // find the maximum flow through the path found.
-                int path_flow = int.MaxValue;
-                for (Vertex v = source; v != sink; v = parents[v.Number])
-                {
-                    Vertex temp = parents[v.Number];
-
-                    if (graph.RawGraph.Edges.Find(t =>
-                        t.FromVertex == parents[v.Number - 1] &&
-                        t.ToVertex == temp) != null)
-                        path_flow
-                            = Math.Min(path_flow,
-                                graph.RawGraph.Edges.Find(t =>
-                                    t.FromVertex == parents[v.Number - 1] &&
-                                    t.ToVertex == temp).Weight - graph.RawGraph.Edges.Find(t =>
-                                    t.FromVertex == parents[v.Number - 1] &&
-                                    t.ToVertex == temp).Capacity);
-                }
-
-                // update residual capacities of the edges and
-                // reverse edges along the path
-                for (Vertex v = source; v != sink; v = parents[v.Number])
-                {
-                    Vertex temp = parents[v.Number - 1];
-                    temp.Edges.Find(t => t.DestinationVertex == v).Capacity += path_flow;
-                }
-
-                // Add path flow to overall flow
-                max_flow += path_flow;
-            }
-
-            // Return the overall flow
-            return max_flow;
         }
     }
 }
--- Graphs1/Program.cs
+++ Graphs1/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Graph
+namespace Graphs1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // var graph = Graph.MakeGraph(
+            //     0, 1,
+            //     0, 2,
+            //     1, 3,
+            //     1, 4,
+            //     2, 3,
+            //     3, 4);
+            //
+            // foreach (var e in graph[0].BreadthSearch())
+            // {
+            //     Console.WriteLine(e.NodeNumber);
+            // }
+            //
+            // foreach (var e in graph[0].DepthSearch())
+            // {
+            //     Console.WriteLine(e.NodeNumber);
+            // }
+            
+            var graph = new Graphs1.Graph(4);
+            var weights = new Dictionary<Edge, double>();
+            weights[graph.Connect(0, 1)] = 1;
+            weights[graph.Connect(0, 2)] = 2;
+            weights[graph.Connect(0, 3)] = 6;
+            weights[graph.Connect(1, 3)] = 4;
+            weights[graph.Connect(2, 3)] = 2;
+
+            var path = Algols.Dijkstra(graph, weights, graph[0], graph[3]).Select(n => n.NodeNumber);
+
+            foreach (var e in path)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
